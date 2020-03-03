497c78566123af906ca56889e264b43ad504f3de
Jacob Shaeffer <jshaeffer@azmag.gov>
2020-03-03 15:45:17 -0700

    added bcrypt for password starage

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get('/recipes', (req, res) => {
@@ -11,3 +14,20 @@ app.get('/recipes', (req, res) => {
 		}
 	});
 });
+
+app.post('/user/create', (req, res) => {
+	let plainText = req.body.password;
+	bcrypt.hash(plainText, saltRounds, (err, hash) => {
+		// Store hash in DB
+	});
+});
+
+app.post('/user/login', (req, res) => {
+	let plainText = req.body.password;
+	let username = req.body.username;
+	let hash = '#'; // Get hash from database
+
+	bcrypt.compare(plainText, hash, (err, result) => {
+		// return = true if plainText is the correct password
+	});
+})
